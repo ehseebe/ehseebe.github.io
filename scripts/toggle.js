bd5968628e6d2796f85ead8fbbd5bbc1700799a2
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-11-23 12:16:08 -0500

    toggle conditional, removed two projects

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
-  console.log("hello!")
+  
   $('.button-vagary').click(function() {
-    console.log("clicked!")
-  $('.vagary').slideDown("slow");
+    if ($('.button-vagary').length) {
+      $('.vagary').slideUp("slow");
+    } else {
+      $('.vagary').slideDown("slow");
+    }
+  
+});
+
+$('.button-teacherest').click(function() {
+  console.log("clicked!")
+$('.teacherest').slideDown("slow");
+});
+
+$('.button-scheduler').click(function() {
+  console.log("clicked!")
+$('.scheduler').slideDown("slow");
+});
+
+$('.button-tweeter').click(function() {
+  console.log("clicked!")
+$('.tweeter').slideDown("slow");
 });
 });
