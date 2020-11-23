43d1bede38aaab8322c126b0464ecb04a6405f17
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2020-11-23 11:11:57 -0500

    jquery edit

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   console.log("hello!")
   $('.button-vagary').click(function() {
     console.log("clicked!")
-  $('.project-details vagary').slideDown("slow");
+  $('.vagary').slideDown("slow");
 });
 });
